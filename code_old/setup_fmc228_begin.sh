3e913f500b35a5f35bc8836cf8beef3bbce50c6f
Ryan H <30587035+ryry013@users.noreply.github.com>
2024-12-11 17:08:24 +0900

    Move old files to backup folders

--- code_old/setup_fmc228_begin.sh
+++ code_old/setup_fmc228_begin.sh
@@ -3,6 +3,10 @@ cat << EOF  |  while read line; do echo "$line" | tee /dev/ttyUSB0; usleep 20000
 arp 192.168.40.2
 arp 
 send 192.168.40.2 48879
+-- below three are set in mu2e_UEM_firmware.srcs/sources_1/xaui_interface.vhd
+-- constant address_icmpreq_sequence : std_logic_vector(15 downto 0) := x"0074";
+-- constant address_icmpreq_daddr : std_logic_vector(15 downto 0) := x"0078";
+-- constant address_icmpreq_timer : std_logic_vector(15 downto 0) := x"007c";
 poke c1000074 c0a82802
 poke c1000078 c001beef
 poke c1000070 40000000
