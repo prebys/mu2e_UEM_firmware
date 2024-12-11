231066dd509cb608cf50ba6d2c5cb2385cf2674f
Ryan H <30587035+ryry013@users.noreply.github.com>
2024-12-11 17:28:17 +0900

    Consolidate old files

--- code_old/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$(readlink -f "$0")")"
-make clean
-make
-./amc502_mux 192.168.40.02 < mux_master.txt
-./amc502_pll 192.168.40.02 < pll_config.txt
-./setup_fmc228_begin.sh
-./setup_fmc228.sh
-sudo ip link set eth1 mtu 9000
