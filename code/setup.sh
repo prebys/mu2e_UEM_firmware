1a9cba434a91bb413d50728de22e337674748c11
Ryan H <30587035+r-hensley@users.noreply.github.com>
2025-01-28 01:14:56 -0600

    Make message more clear about if mux configured properly

--- code/setup.sh
+++ code/setup.sh
@@ -2,12 +2,32 @@
 cd "$(dirname "$(readlink -f "$0")")"
 # make clean
 # make
+
 sudo ip link set eth1 mtu 9000
-firewall-cmd --permanent --add-port=48879/udp
+sudo firewall-cmd --permanent --add-port=48879/udp
+
+
 while true; do
+	ping 192.168.40.245 -c 1
 	./amc502_mux 192.168.40.245 < mux_master.txt
-	echo "Did the mux configure properly? You should see a mix of 'INPUT' and 'OUTPUT'. Press 'n' to try again, or any other key to continue."
-	read -t 5 -n 1 user_input
+	./amc502_mux 192.168.40.245 < mux_master.txt
+	echo -e "\n\n"
+	echo "-- Did the mux configure properly?" 
+	echo "-- You should see a mix of 'INPUT' and 'OUTPUT'. "
+	echo "----------------"
+	echo "-- Good Example:"
+	echo "-- MUX1 =  40 84 00 00 40 00 00 00 00 00 84 00 00 00 84 00"
+	echo "--   Port 00 (FMCEXPA) :  input Z=100 Ohm"
+	echo "--   Port 01 (FMCEXPB) :  output <-- 4 (PLLCLKA_out)"
+	echo "--   [ ... ]"
+	echo "----------------"
+	echo "-- Bad Example:"
+	echo "-- MUX1 =  ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff"
+	echo "--   Port 00 (FMCEXPA) :  output <-- 15 (FMC1_CLK3_BIDIR)"
+	echo "--   Port 01 (FMCEXPB) :  output <-- 15 (FMC1_CLK3_BIDIR)"
+	echo -e "--\n--"
+	echo "-- Press 'n' to try again, or any other key to continue."
+	read -t 15 -n 1 user_input
 	
 	if [ $? -eq 0 ]; then
 		if [[ "$user_input" =~ ^[Nn]$ ]]; then
