63de94c72caa737f7eaac5199ffb2c10c018fe8f
Ryan H <30587035+r-hensley@users.noreply.github.com>
2025-05-18 21:50:30 -0700

    Fix up sdk directory and test rerunning files

---
+++ vivado_project/src/sdk/monitor_amc502/Debug/src/monitor_fmc228_pcie.d
@@ -0,0 +1,27 @@
+src/monitor_fmc228_pcie.o src/monitor_fmc228_pcie.o: \
+ ../src/monitor_fmc228_pcie.c ../src/platform.h ../src/platform_config.h \
+ ../../standalone_bsp_0/microblaze_I/include/mb_interface.h \
+ ../../standalone_bsp_0/microblaze_I/include/xil_types.h \
+ ../../standalone_bsp_0/microblaze_I/include/xil_assert.h \
+ ../../standalone_bsp_0/microblaze_I/include/xil_exception.h \
+ ../src/kernel.h ../src/lmk04824.h ../src/hmc835.h ../src/ad9234.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../standalone_bsp_0/microblaze_I/include/mb_interface.h:
+
+../../standalone_bsp_0/microblaze_I/include/xil_types.h:
+
+../../standalone_bsp_0/microblaze_I/include/xil_assert.h:
+
+../../standalone_bsp_0/microblaze_I/include/xil_exception.h:
+
+../src/kernel.h:
+
+../src/lmk04824.h:
+
+../src/hmc835.h:
+
+../src/ad9234.h:
