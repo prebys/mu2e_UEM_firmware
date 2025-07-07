08c93f79ee8457883e0bf699da519c4e955c721b
Mu2eUEM <30587035+r-hensley@users.noreply.github.com>
2025-07-07 09:26:52 -0500

    Delete old files

--- socketudp/test/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source /disk2/miniconda3/etc/profile.d/conda.sh
-conda activate base
-echo "which python3 in shell script "
-which python3
-python3 test.py
--- socketudp/test/test2.sh
+++
@@ -1,1 +0,0 @@
-sudo ./test.sh
